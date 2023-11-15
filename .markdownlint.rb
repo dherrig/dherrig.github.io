66a94d48de7ae0819e839ccb53bc15d24b1ae3d3
David Herrig <david.herrig@gmail.com>
2023-11-15 13:44:46 -0700

    remove jekyll relative URL check and some md rules

--- .markdownlint.rb
+++ .markdownlint.rb
@@ -1,2 +1,10 @@
 all
+# MD002 First header should be a top level header
+# Jekyll's title is the h1 on the page. The first heading in a post should be the h2
+exclude_rule 'MD002'
+# MD033 Inline HTML
+# I need inline html sometimes for jekyll markdown
 exclude_rule 'MD033'
+# MD041 First line in file should be a top level header
+# Excluded because Jekyll files have a preamble
+exclude_rule 'MD041'
